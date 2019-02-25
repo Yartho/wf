7fb83d37b2b600b76e8cd8c3a60bf3765bc1d338
yannhoffmann <yann.hoffmann@icloud.com>
2019-02-25 09:49:05 +0100

    corrected version exercice

--- PHP/Exercice1/exercise.php
+++ PHP/Exercice1/exercise.php
@@ -4,20 +4,20 @@ $booleanTrue=true;
 $booleanFalse=false;
 
 $int=2019991992;
-a
+
 $float=2.15;
 
 $integer=1455000033;
 
-$double= [4,445555];
+$double= 4.445555;
 
 $string='Hello';
 
-$array=['Hello','World'];
+$array=[1,2,'3'];
 
-$doubleDimensionArray=['Hello','Morning'];
+$doubleDimenssionArray=  [[1,2],[4,5]];
 
-$associativeArray=[ 'luxembourg' => [12233445455,23333333444],
-                'trier' => [2333444444,23343445555]];
+$associativeArray=[ 'luxembourg' => [12233445455,23333333444]];
+
 
 $null=null;
