2789a73a702178bba0df9bfebeed7b435f795040
yannhoffmann <yann.hoffmann@icloud.com>
2019-02-25 15:11:00 +0100

    Exercise cards

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -2,3 +2,15 @@
 
 $input;
 $winner;
+$BrotherA =0;
+$BrotherB = 0;
+foreach ($input as $round) { if( $round [0] > $round [1]) { $BrotherA++;}
+    else {
+        if ($round [0] < $round[1]) {
+            $BrotherB++;
+        };
+    }
+}
+
+if ($BrotherA > $BrotherB){$winner = "A";}
+else {$winner = "B";}
