a65e26b2f5d13e9765223c433bbc37e02afd7e1f
yannhoffmann <yann.hoffmann@icloud.com>
2019-02-26 16:01:41 +0100

    Exercice namespace

--- PHP/Exercice5/src/DimensionalMath/Vector/AngleCalculation.php
+++ PHP/Exercice5/src/DimensionalMath/Vector/AngleCalculation.php
@@ -1,13 +1,16 @@
 <?php
 
-function getVectorAngle($vectorA, $vectorB) {
-   
-    $pi=pi();
-   
-    list($ax, $ay) = $vectorA;
-    list($bx, $by) = $vectorB;
-    
-    $i=(acos((($ax*$ay)+($bx*$by))/(sqrt(($ax*$ax)+($bx*$bx))*sqrt(($ay*$ay)+($by*$by))))*360)/($pi*2);
-   
-    return $i;
-}
+namespace DimensionalMath\Vector;
+
+    function getVectorAngle($vectorA, $vectorB)
+    {
+
+        $pi = pi();
+
+        list($ax, $ay) = $vectorA;
+        list($bx, $by) = $vectorB;
+
+        $i = (acos((($ax * $ay) + ($bx * $by)) / (sqrt(($ax * $ax) + ($bx * $bx)) * sqrt(($ay * $ay) + ($by * $by)))) * 360) / ($pi * 2);
+
+        return $i;
+    }
