9ee21ef10eb0578c5ccd27699603e4ba38fa7183
yannhoffmann <yann.hoffmann@icloud.com>
2019-02-28 12:29:32 +0100

    Easter part 2

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,20 @@
 <?php
+ $filenameOriginal = "file.txt";
+ $filenameToWrite = "fileTemp.txt";
+
+ copy ($filenameOriginal, $filenameToWrite);
+
+ $fileContent = file_get_contents($filenameToWrite);
+
+ echo "$fileContent";
+
+ echo "<br> Halfway :".floor(strlen($fileContent)/2);
+
+ $secondPart = substr($fileContent, floor(strlen($fileContent)/2));
+ $firstPart = substr($fileContent,0,strlen($secondPart) -1);
+
+ echo "<br>firstPart:<br>".$firstPart;
+
+ echo "<br>secondPart:<br>".$secondPart;
+
+ echo "<br>secondPartReversed:<br>".strrev($secondPart);
