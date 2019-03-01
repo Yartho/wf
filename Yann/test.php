b34074cdc6c8671b61c79a003668e153bb1d94b1
yannhoffmann <yann.hoffmann@icloud.com>
2019-03-01 12:48:39 +0100

    Correction test

--- Yann/test.php
+++ Yann/test.php
@@ -8,15 +8,37 @@ function AddStringToArray ($ArrayIn, $StringIn){
 
 }
 
- function AddStringToArrayExtra ($ArrayIn, $StringIn){
+function AddStringToArrayExtra ($ArrayIn, $StringIn){
+$ArrayIn["newArray"] = $StringIn;
+    return $ArrayIn;
 
 
 
 
- }
+
+}
+
+
+$MyArray = ['John','Mary'];
 
 
 
- $MyArray = ['John','Mary'];
 
-print_r (AddStringToArray ($MyArray, "Joe"));
+//print_r (AddStringToArray ($MyArray, "Joe"));
+//$MyArray = AddStringToArray($MyArray, 'Hello');
+//print_r ($MyArray);
+
+function FindInArrayAndChange ($ArrayIn, $StringToFind,$NewValue){
+  $key =array_search($StringToFind,$ArrayIn );
+  $ArrayIn[$key] = $NewValue;
+
+  return $ArrayIn;
+
+
+
+
+};
+
+$MyArray = ['John','Mary']; 
+
+print_r(FindInArrayAndCHange ($MyArray, 'John','xxx'));
