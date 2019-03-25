4cf94887d3c7409fb7f4a97c856dd7946134300b
yannhoffmann <yann.hoffmann@icloud.com>
2019-03-25 16:31:57 +0100

    exercice 2 correct

--- OOP/Exercice2/src/Model/User.php
+++ OOP/Exercice2/src/Model/User.php
@@ -1,6 +1,5 @@
 <?php
 namespace Model;
-
 class User
 {
     private $id;
@@ -8,60 +7,73 @@ class User
     protected $password;
     protected $salt;
     protected $username;
-    
+
     public function getId()
     {
         return $this->id;
     }
-
     public function getRoles()
     {
-        return $this->roles;
-    }
+        $labels = [Role::ROLE_USER];
+        foreach ($this ->roles as $role)
+        {array_push($labels, $role ->getlabel());
 
+        }
+
+
+        return array_unique($labels);
+    }
     public function getPassword()
     {
         return $this->password;
     }
-
     public function getSalt()
     {
         return $this->salt;
     }
-
     public function getUsername()
     {
         return $this->username;
     }
-
-    public function setRoles($roles)
+    public function setRoles(array $roles)
     {
-        $this->roles = $roles;
+        $this->roles = [];
+
+       foreach  ($roles as $role)
+        {
+            $this->addRole($role);
+        }
         return $this;
     }
 
+    public function addRole(Role $role)
+    {
+        if (!in_array($role, $this->roles)) {
+            array_push($this->roles, $role);
+        }
+
+        return $this;
+    }
     public function setPassword($password)
     {
         $this->password = $password;
         return $this;
     }
-
     public function setSalt($salt)
     {
         $this->salt = $salt;
         return $this;
     }
-
     public function setUsername($username)
     {
         $this->username = $username;
         return $this;
     }
-
     public function eraseCredentials()
     {
         $this->password = null;
         $this->salt = null;
     }
-}
 
+
+}
